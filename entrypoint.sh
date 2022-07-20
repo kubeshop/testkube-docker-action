8fa3af88981ceefb05f226567a2daa4b59bfe6f8
Vladislav Sukhin <vladislav@kubeshop.io>
2022-07-20 22:00:43 +0300

    fix: check exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -l
 
 result=`kubectl testkube $1 $2 $3`
+
+status=$?
+if [[ $status -eq 1 ]]; then
+  exit 1
+fi
+
 result="${result//'%'/'%25'}"
 result="${result//$'\n'/'%0A'}"
 result="${result//$'\r'/'%0D'}"
